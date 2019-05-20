b979e4b1da4352816855e98a1f8e3480c28eb11e
ollinieminen <otn987@gmail.com>
2019-05-20 11:11:40 -0700

    create red blue yellow components

---
+++ src/component/Blue.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Blue(){
+  return (
+    <div style={{ width:'100px', height:'100px', backgroundColor:'blue' }}>
+
+    </div>
+  );
+}
---
+++ src/component/Red.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Red(){
+  return (
+    <div style={{ width:'100px', height:'100px', backgroundColor:'red' }}>
+
+    </div>
+  );
+}
